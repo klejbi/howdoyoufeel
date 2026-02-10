0236f0305eb522881bed943210c0e2a58ef170fb
Oliwia Kleybor <kleyboro@gmail.com>
2026-02-10 22:57:09 +0100

    no back to homepage

--- lib/homepage.dart/homepage.dart
+++ lib/homepage.dart/homepage.dart
@@ -30,7 +30,7 @@ class HomePage extends StatelessWidget {
               elevation: 0,
             ),
             onPressed: () {
-              Navigator.push(
+              Navigator.pushReplacement(
                 context,
                 MaterialPageRoute(builder: (context) => const Emotions()),
               );
